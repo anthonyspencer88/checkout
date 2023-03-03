b32ed1434baa3dbec955296aab99f73a03cacc39
Johannes Schindelin <johannes.schindelin@gmx.de>
2023-03-03 11:58:31 +0100

    TEST: import coreCommand again, as the unexplained load time dropped dramatically
    There is a noticeable delay in `actions/checkout@v3` after this line:
    
    	▶ Run dscho/checkout@main
    
    The delay is diagnosed by instrumenting the compiled Action: instead of
    starting the core logic we throw an exception.
    
    With an only lightly instrumented version, we see an almost two minute
    long delay after that line:
    https://github.com/dscho/git-for-windows-automation/actions/runs/4321187056/jobs/7542820362#step:2:49
    
    Removing some imports at first did not change that, but when removing
    all but the first import, the delay dropped to two seconds:
    https://github.com/dscho/git-for-windows-automation/actions/runs/4321187056/jobs/7545290126#step:2:49
    
    Let's see whether loading `coreCommand` is what causes that surprising
    difference.
    
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- dist/index.js
+++ dist/index.js
@@ -4643,6 +4643,7 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const core = __importStar(__webpack_require__(470));
+const coreCommand = __importStar(__webpack_require__(431));
 // Main
 console.log(new Error(`Uh oh!`))
 throw new Error(`TEST!!!`)
